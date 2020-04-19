661af0bda544d3b4395d7d4dd98b30bf6af174f1
konapun <bremen.braun@gmail.com>
2020-04-19 14:16:35 -0500

    Updates

--- src/components/data/DataProvider.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import DataContext from './DataContext'
-import config from '../../../config'
-
-export default ({ children }) => (
-  <DataContext.Provider value={config}>
-    {children}
-  </DataContext.Provider>
-)
