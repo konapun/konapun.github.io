5375808daf2593f0fffb1c058a506c7e02cada5e
konapun <bremen.braun@gmail.com>
2021-02-04 00:24:09 -0600

    fix missing template file caused by gitignore rule

---
+++ src/templates/tags.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import BlogLayout from '../components/layout/BlogLayout'
+
+export default ({ data, pageContext }) => {
+  const { tag } = pageContext
+  const posts = data.allMarkdownRemark.edges
+  const title = `Posts tagged with "${tag}"`
+
+  // TODO: make into reusable component
+  return (
+    <BlogLayout posts={posts} seo={title}>
+      <h2>{title}</h2>
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        console.log('Post:', node)
+        return (
+          <article key={node.fields.slug}>
+            <header>
+              <h3>
+                <Link style={{ boxShadow: `none` }} to={`/blog${node.fields.slug}`}> {/* FIXME */}
+                  {title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+            </header>
+            <section>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </section>
+          </article>
+        )
+      })}
+    </BlogLayout>
+  )
+}
+
+export const pageQuery = graphql`
+  query($tag: String) {
+    allMarkdownRemark(
+      limit: 2000
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { tags: { in: [$tag] } } }
+    ) {
+      totalCount
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
